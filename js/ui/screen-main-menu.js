398fdb316bf0918534d893adf00a1ed1f1e05826
LeranMora <hickeyteary@gmail.com>
2026-05-02 19:28:32 +0700

    Update screen-main-menu.js

--- js/ui/screen-main-menu.js
+++ js/ui/screen-main-menu.js
@@ -6,15 +6,12 @@ export function returnMainMenu() {
 }
 
 export function showMainMenu() {
-  console.log('showMainMenu вызвана!');
   const screen = document.getElementById('screen-main-menu');
-  console.log('screen найден:', screen);
   if (screen) {
     screen.innerHTML = `
       <h2>Добро пожаловать в «Мандрагору», молодой алхимик!</h2>
       <p>Это место - ваша личная лавка, где вы можете прокачивать свой скилл алхимии и продавать собственные зелья и алхимические субстанции!</p>
       <h3>Удачи в вашем приключении, Алхимик!</h3>`;
     screen.style.display = 'block';
-    console.log('Главное меню показано!');
   }
 }
