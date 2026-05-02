03fbd5b01420a4744cce2345da5436c6bcf4cc2b
LeranMora <hickeyteary@gmail.com>
2026-05-02 14:14:33 +0700

    list-potions.js

--- js/list-potions.js
+++ js/list-potions.js
@@ -1,1 +1,25 @@
-
+function openRecipeBook() {
+  const ingrefients = [
+    { id: 'rose', name: 'Лепестки роз', price: '7' },
+    { id: 'moon-dust', name: 'Лунная пыльца', price: '5' },
+    { id: 'unicorn-tea', name: 'Слеза единорога', price: '10' },
+    { id: 'aloe', name: 'Сок алоэ', price: '7' },
+    { id: 'mandrake', name: 'Корень мандрагоры', price: '5' },
+    { id: 'spring-water', name: 'Родниковая вода', price: '3' },
+    { id: 'honey', name: 'Мёд', price: '7' },
+    { id: 'soap-root', name: 'Мыльный корень', price: '7' },
+    { id: 'lemon-jutci', name: 'Лимонный сок', price: '3' },
+    { id: 'crystal-salt', name: 'Кристальная соль', price: '5' },
+    { id: 'sun-drop', name: 'Капля солнца', price: '10' },
+    { id: 'firefly', name: 'Пыльца светлячков', price: '7' },
+    { id: 'glow-mushroom', name: 'Люминесцентный гриб', price: '5' }
+  ];
+  
+  const potions = [
+    {
+      id: 'love',
+      name: 'Зелье любви', 
+      price: 50, 
+      description: 'Помогает в делах сердечных, возвращает утерянную любовь.',
+      ingredients: []
+}
