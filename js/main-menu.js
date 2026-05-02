3dac45fd045afb949a478790c92e22aabbc3b0d2
LeranMora <hickeyteary@gmail.com>
2026-05-02 16:23:20 +0700

    Update main-menu.js

--- js/main-menu.js
+++ js/main-menu.js
@@ -1,6 +1,6 @@
 export function returnMainMenu {
   const openMainMenu = document.getElementByIf('main-menu-wn');
   if () {
-    openMainMenu.style.display = 'none';
+    openMainMenu.style.display = 'block';
   }
 }
