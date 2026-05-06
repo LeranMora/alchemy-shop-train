8c59c0648de0884508cc677e0c03485c2586f73c
LeranMora <hickeyteary@gmail.com>
2026-05-06 13:14:09 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // 2. Находим кнопки в основном файле. Т.к. этот файл подключен к основному .html и все что мы ищем через этот код, мы ищем сразу в этом файле
 const listBtn = document.getElementById('list-menu');
-const returnBtn = document.getElementById('main-menu-btn');
+//const returnBtn = document.getElementById('main-menu-btn');
 // const startBtn = document.getElementById('start-game');
 
 // 3. Вешаем обработчики, проверяем заполнена ли переменная данными (null или нет). Данный вариант упрощенный и корректный, приравнивается к listBtn !=== null
