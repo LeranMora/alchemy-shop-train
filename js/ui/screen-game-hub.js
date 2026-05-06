e99c783995bb0ac7fab116002a6eca683794c8cb
LeranMora <hickeyteary@gmail.com>
2026-05-06 20:54:58 +0700

    Update screen-game-hub.js

--- js/ui/screen-game-hub.js
+++ js/ui/screen-game-hub.js
@@ -1,27 +1,39 @@
 const screenHeader = document.getElementById('header-game-hub');
+const screenFooter = document.getElementById('footer-game-hub');
 export function openGameHub() {
-  if (screenHeader) {
-    screenHeader.innerHTML = `
-    <button id="save-game" class="layout-btn">Сохранить игру</button>
-    <button id="load-game" class="layout-btn">Удалить игру</button>
-    <button id="main-menu-btn" class="layout-btn">Главное меню</button>
-    `;
-    screenHeader.style.display = 'grid';
-    const returnBtn = document.getElementById('main-menu-btn');
-        if (returnBtn) {
-            returnBtn.addEventListener('click', returnMainMenu);
-        }
-  }
-}
-export function returnMainMenu() {
-    const openMainMenu = document.getElementById('screen-main-menu');
-    const errorMsg = document.getElementById('errorMessage');
-    const playerInput = document.getElementById('playerNameInput');
-    console.log('Нашли элемент с вводом имени');
-    if (openMainMenu && errorMsg && screenHeader) {
-      openMainMenu.style.display = 'block';
-      errorMsg.style.display = 'none';
-      playerInput.value = ''; // Очищаем текст внутри поля
-      screenHeader.style.display = 'none';
+  screenHeader.innerHTML = `
+  <button id="save-game" class="layout-btn">Сохранить игру</button>
+  <button id="load-game" class="layout-btn">Удалить игру</button>
+  <button id="main-menu-btn" class="layout-btn">Главное меню</button>
+  `;
+  screenFooter.innerHTML = `
+  <div>
+  <div>lvl: 0 (0 / 100)</div>
+    <div>Gold: 100</div>
+</div>
+<div>
+  <div>Репутация лавки: 0</div>
+  <div>✾ Чистота лавки ✾: 0</div>
+</div>
+  `;
+  screenHeader.style.display = 'grid';
+  screenFooter.style.display = 'grid';
+  const returnBtn = document.getElementById('main-menu-btn');
+    if (returnBtn) {
+      returnBtn.addEventListener('click', returnMainMenu);
     }
+  }
+
+export function returnMainMenu() {
+  const openMainMenu = document.getElementById('screen-main-menu');
+  const errorMsg = document.getElementById('errorMessage');
+  const playerInput = document.getElementById('playerNameInput');
+    
+  if (!openMainMenu || !errorMsg || !playerInput) return;
+    
+  openMainMenu.style.display = 'block';
+  errorMsg.style.display = 'none';
+  playerInput.value = ''; // Очищаем текст внутри поля
+  screenHeader.style.display = 'none';
 }
+
