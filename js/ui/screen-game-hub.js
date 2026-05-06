ab75dea4e9053ace539aa64b87c3de936b9ca34e
LeranMora <hickeyteary@gmail.com>
2026-05-06 13:17:18 +0700

    Update screen-game-hub.js

--- js/ui/screen-game-hub.js
+++ js/ui/screen-game-hub.js
@@ -1,17 +1,11 @@
-<button id="save-game" class="layout-btn" style="display:none">Сохранить игру</button>
-<button id="load-game" class="layout-btn" style="display:none">Загрузить игру</button>
-<button id="list-menu" class="layout-btn" style="display:none">Книга рецептов</button>
-<button id="close" class="layout-btn" style="display:none">Назад</button>
-<button id="delete-game" class="layout-btn" style="display:none"> Удалить игру</button>
-<button id="statistics-game" class:"layout-btn" style="display:none">Статистика</button>
-
-
-  <div>
-  <div>lvl: 0 (0 / 100)</div>
-    <div>Gold: 100</div>
-</div>
-<div>
-  <div>Репутация лавки: 0</div>
-  <div>✾ Чистота лавки ✾: 0</div>
-</div>
-<button id="main-menu-btn" class="layout-btn">Главное меню</button>
+export function openGameHub() {
+  const screenHeader = document.getElementById('header-game-hub');
+  if (screenHeader) {
+    screenHeader.innerHTML = `
+    <button id="save-game" class="layout-btn" style="display:none">Сохранить игру</button>
+    <button id="load-game" class="layout-btn" style="display:none">Загрузить игру</button>
+    <button id="list-menu" class="layout-btn" style="display:none">Книга рецептов</button>
+    <button id="statistics-game" class:"layout-btn" style="display:none">Статистика</button>
+    `;
+  }
+}
