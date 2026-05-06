5858e7acb4c0618482c946d7028c342ee3fea35c
LeranMora <hickeyteary@gmail.com>
2026-05-06 13:33:40 +0700

    Update screen-game-hub.js

--- js/ui/screen-game-hub.js
+++ js/ui/screen-game-hub.js
@@ -3,9 +3,9 @@ export function openGameHub() {
   if (screenHeader) {
     screenHeader.innerHTML = `
     <button id="save-game" class="layout-btn" style="display:none">Сохранить игру</button>
-    <button id="load-game" class="layout-btn" style="display:none">Загрузить игру</button>
-    <button id="list-menu" class="layout-btn" style="display:none">Книга рецептов</button>
-    <button id="statistics-game" class="layout-btn" style="display:none">Статистика</button>
+    <button id="load-game" class="layout-btn" style="display:none">Удалить игру</button>
+    <button id="list-menu" class="layout-btn" style="display:none">Главное меню</button>
     `;
+    screenHeader.style.display = 'block';
   }
 }
